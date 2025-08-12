e73d39863259aec1afa4b9cfc630b252f08cc7f3
Erica Burgos <ericab79@gmail.com>
2025-08-12 12:33:24 +0000

    That looks nice, but I don't think I like the use of the  border

--- src/components/app/idiom-card.tsx
+++ src/components/app/idiom-card.tsx
@@ -137,10 +137,10 @@ export function IdiomCard({ idiom }: IdiomCardProps) {
         </div>
       </CardContent>
 
-      <CardFooter className="p-0 border-t">
+      <CardFooter className="p-0">
         <Sheet>
           <SheetTrigger asChild>
-            <Button variant="outline" className="w-full border-t-0 border-x-0 rounded-t-none">
+            <Button variant="ghost" className="w-full rounded-t-none">
               <Info className="w-5 h-5 mr-2" />
               Details & Meaning
             </Button>
