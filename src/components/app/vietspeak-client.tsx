a748c89273bafbbec29c8c3432dc68703e7bcb58
Erica Burgos <ericab79@gmail.com>
2025-08-12 11:04:52 +0000

    I am unable to scroll down to view the DETAILS & MEANING, update the int

--- src/components/app/vietspeak-client.tsx
+++ src/components/app/vietspeak-client.tsx
@@ -7,22 +7,24 @@ import { Header } from "./header";
 
 export function VietSpeakClient() {
     return (
-        <div className="flex flex-col h-full overflow-hidden">
+        <div className="flex flex-col h-full">
             <Header />
-            <div className="flex-grow flex items-center justify-center p-4">
-                <Carousel className="w-full max-w-lg" opts={{ loop: true }}>
-                    <CarouselContent className="-ml-2">
-                        {idioms.map((idiom) => (
-                            <CarouselItem key={idiom.id} className="pl-2">
-                                <div className="p-1">
-                                    <IdiomCard idiom={idiom} />
-                                </div>
-                            </CarouselItem>
-                        ))}
-                    </CarouselContent>
-                    <CarouselPrevious className="ml-[-8px]" />
-                    <CarouselNext className="mr-[-8px]" />
-                </Carousel>
+            <div className="flex-grow overflow-y-auto p-4">
+                <div className="flex items-center justify-center min-h-full">
+                    <Carousel className="w-full max-w-lg" opts={{ loop: true }}>
+                        <CarouselContent className="-ml-2">
+                            {idioms.map((idiom) => (
+                                <CarouselItem key={idiom.id} className="pl-2">
+                                    <div className="p-1">
+                                        <IdiomCard idiom={idiom} />
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious className="ml-[-8px]" />
+                        <CarouselNext className="mr-[-8px]" />
+                    </Carousel>
+                </div>
             </div>
         </div>
     )
